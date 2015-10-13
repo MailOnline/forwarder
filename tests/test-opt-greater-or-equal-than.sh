fda3ae143fddb24aa5d65c33f3e34d99f72cb773
Fabio Manera <fabio.manera@mailonline.co.uk>
2015-10-13 15:55:00 +0100

    add test for --greater-or-equal-than (with 1 failing test)

---
+++ tests/test-opt-greater-or-equal-than.sh
@@ -0,0 +1,11 @@
+for i in {1..10}; do log_random >> myapp.log; done
+
+log_random >> myapp.log
+ts=$(last_timestamp)
+log_random >> myapp.log
+log_random >> myapp.log
+
+assert "sender -g '$ts' 'myapp.*' | count_lines" 3
+assert "sender --greater-or-equal-than '$ts' -o offsets 'myapp.*' | count_lines" 3
+assert "sender --greater-or-equal-than '$ts' -o offsets 'myapp.*' | count_lines" 0
+assert "sender -o offsets 'myapp.*' | count_lines" 0
