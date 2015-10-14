fff7b2c488351c66e323883c368d9de399dfc6b6
Fabio Manera <fabio.manera@mailonline.co.uk>
2015-10-14 17:33:27 +0100

    remove extra bytes from uuid when it comes from python script

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@
 
 function log_random() {
 	now=$(gdate +"%Y-%M-%dT%T.%N" 2>/dev/null || date +"%Y-%M-%dT%T.%N")
-	uuid=$( ( uuidgen 2>/dev/null || python  -c 'import uuid; print uuid.uuid1(), "(from python)"' ) 2>/dev/null )
+	uuid=$( ( uuidgen 2>/dev/null || python  -c 'import uuid; print uuid.uuid1()' ) 2>/dev/null )
 	echo $uuid > .last_uuid
 	echo $now > .last_timestamp
 	echo "$now And this is a UUID just to ensure this line is unique: $uuid"
