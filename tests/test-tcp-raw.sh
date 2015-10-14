59522c115b3bd7b152e96a0baa4ab8e0eb20995e
Fabio Manera <fabio.manera@mailonline.co.uk>
2015-10-14 20:29:22 +0100

    force kill tcp server and some code gardening

--- tests/test-tcp-raw.sh
+++ tests/test-tcp-raw.sh
@@ -1,6 +1,10 @@
 for i in {1..10}; do log_random >> myapp.log; done
 
-( [ `uname` == "Darwin" ] && nc -l -p 19501 || nc -l 19501 ) > received.log & 
+if [ `uname` = "Darwin" ]; then
+	nc -l -p 19501 > received.log & 
+else 
+	nc -l 19501 > received.log &
+fi
 
 server_pid=$!
 
@@ -9,3 +13,5 @@ sleep 0.1
 sender 'myapp.*' -t localhost:19501
 
 assert "count_lines received.log" 10
+
+kill -9 $server_pid &> /dev/null || true
