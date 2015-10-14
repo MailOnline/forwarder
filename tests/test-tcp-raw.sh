10d0a2e14ec44993d64d4443483c99d426474fb7
Fabio Manera <fabio.manera@mailonline.co.uk>
2015-10-14 19:55:31 +0100

    add test for raw tcp connections (like sending directly to splunk)

---
+++ tests/test-tcp-raw.sh
@@ -0,0 +1,10 @@
+for i in {1..10}; do log_random >> myapp.log; done
+
+netcat -l -p 19501 > received.log &
+server_pid=$!
+
+sleep 0.1
+
+sender 'myapp.*' -t localhost:19501
+
+assert "count_lines received.log" 10
