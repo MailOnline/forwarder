6657cf6398ab3fe16072fd01414bed830d7d506f
Tom Burnell <tombburnell@gmail.com>
2017-02-03 17:23:07 +0000

    add test for METRIC forwarder.duplicatesig

--- tests/test-signature-size.sh
+++ tests/test-signature-size.sh
@@ -3,3 +3,12 @@ echo "AAAAAAAAAAAAAAAAAA CCCCCCCCCCCCCCC" > myapp.log.1
 
 assert "sender -s 15 'myapp.*' | count_lines" 0
 assert "sender -s 30 'myapp.*' | count_lines" 2
+
+echo "AAAAAAAAAAAAAAAAAA DDDDDDDDDDDDDDD" > myapp.log.2
+echo "AAAAAAAAAAAAAAAAAA DDDDDDDDDDDDDDD" > myapp.log.3
+echo "AAAAAAAAAAAAAAAAAA DDDDDDDDDDDDDDD" > myapp.log.4
+echo "AAAAAAAAAAAAAAAAAA DDDDDDDDDDDDDDD" > myapp.log.5
+
+assert "sender -s 30 'myapp.*' | count_lines" 2
+
+assert "sender -s 30 'myapp.*' 2>&1 | grep forwarder.duplicatesig | count_lines" 3
