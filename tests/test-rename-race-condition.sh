93522afe975d8f722594b8d5addf07785b6e3cd8
Fabio Manera <fabio.manera@mailonline.co.uk>
2024-10-08 18:27:07 +0100

    test: test not failing

--- tests/test-rename-race-condition.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..10}; do log_random >> myapp.log; done
-
-assert "sender -p offsets 'myapp.*' | count_lines" 10
-
-mv myapp.log myapp.log.1
-
-log_random >> myapp.log
-
-assert "sender -p offsets 'myapp.*' | count_lines" 1
